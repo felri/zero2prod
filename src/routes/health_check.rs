47938450c2bceb183db299aa84cc97e6bcbca969
felri <felipercmelo@gmail.com>
2024-05-31 20:37:13 -0300

    add logs

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,6 @@
 use actix_web::{HttpResponse, Responder};
 
+#[tracing::instrument(name = "Checking service health", skip())]
 pub async fn health_check() -> impl Responder {
-  HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish()
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,20 @@
+use once_cell::sync::Lazy;
 use reqwest;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
+static TRACING: Lazy<()> = Lazy::new(|| {
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber("test".into(), "debug".into(), std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber("test".into(), "info".into(), std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -80,6 +93,10 @@ async fn subscribe_returns_a_400_for_invalid_data() {
 }
 
 async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
